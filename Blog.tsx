d8821def9243cfd24fa9d98120ad6599be7f9265
aissacoin <aissacoin@gmail.com>
2026-01-03 15:12:18 +0100

    Update Blog.tsx

--- Blog.tsx
+++ Blog.tsx
@@ -1,5 +1,4 @@
 
-import React, { useEffect, useState } from 'react';
 import { BookOpen, Clock, ArrowRight, Search, Loader2, Sparkles, History } from 'lucide-react';
 import { getDailyChronicles } from '../services/geminiService';
 
